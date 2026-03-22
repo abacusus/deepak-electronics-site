7c6877c57f6423139e56af5b4e1cba619130e48b
abacusus <namankumar0063@gmail.com>
2026-03-22 11:46:34 +0530

    minor changes for deployment

--- server/server.js
+++ server/server.js
@@ -357,4 +357,8 @@ app.put("/api/getorders/:id", async (req, res) => {
 });
 
 //  server
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
